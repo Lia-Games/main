654203365237e619e74b85fe38d96114f8e76b41
Lia-Games <146239580+Lia-Games@users.noreply.github.com>
2023-10-04 13:49:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+if(document.referrer != "https://ahlyialogin.github.io/"){
+  window.location.href = "https://ahlyialogin.github.io/"
+}
+
+function fruitButton(){
+  window.locaion.href = "google.com"
+}
