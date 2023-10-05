dc35b73476d504117f65a9c96c40bff79d2bc9ae
Lia-Games <146239580+Lia-Games@users.noreply.github.com>
2023-10-04 20:19:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 if(document.referrer != "https://ahlyialogin.github.io/"){
-  if(document.referrer != "https://lia-games.githu.io/assets/gamefiles/fruitninja/index.html"){
+  if(document.referrer != "https://lia-games.githu.io/assets/gamefiles/fruitninja/"){
     window.location.href = "https://ahlyialogin.github.io/";
   }
 }
 
 function fruitButton(){
-  window.location.href = "https://lia-games.github.io/assets/gamefiles/fruitninja/index.html";
+  window.location.href = "https://lia-games.github.io/assets/gamefiles/fruitninja/";
 }
